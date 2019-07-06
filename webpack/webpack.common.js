27493a33d310d9654de18548e84c732c58866f6e
Vincent Gaviria <vingaviria@gmail.com>
2019-07-06 02:47:22 -0400

    [Add] Webpack visualizer plugin
     - Added the webpack visualizer plugin to get stats on the sizes of the
     js bundles getting created. The goal here is to use this tool to help
     find every opportunity to reduce the size of the bundle. Right now it
     is sitting extremely high (~500kb minified), which is mostly due to the
     `emoji.json` package. There are some easy fixes we can do to reduce
     this by at least half, and even more optimizations to remove
     dependencies on `core-js`.

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,6 +2,7 @@ const Path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VisualizerPlugin = require('webpack-visualizer-plugin');
 
 module.exports = {
   entry: {
@@ -24,7 +25,8 @@ module.exports = {
     ]),
     new HtmlWebpackPlugin({
       template: Path.resolve(__dirname, '../src/index.html')
-    })
+    }),
+    new VisualizerPlugin()
   ],
   resolve: {
     alias: {
